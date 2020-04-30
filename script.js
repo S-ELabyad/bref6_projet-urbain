3d12a778cc78b1669eb1b5ba294b28832c31467b
saloua <salouaelabyad@gmail.com>
2020-04-30 17:35:25 +0000

    commit

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 /*server local*/
-var express = require("express"),
-fs = require('fs'),
-app = express(),
-personne = "",
-http = require("http").Server(app).listen(80);
-app.use("/css",express.static("./css"))
-app.use("/Traitement",express.static("./Traitement"))
-app.use("/img",express.static("./img"))
-app.get("/inscription.html",function(req,res){ 
+const _EXPRESS = require('express');
+fs = require('fs');
+const _APP = _EXPRESS();
+const _BODY_PARSER = require('body-parser');
+const _PATH = require('path');
+const _personne = ""; 
+const _PORT = 3003;
+
+
+
+_APP.use("/css",express.static("./css"));
+_APP.get("/inscription.html",function(req,res){ 
 fs.readFile('JSON/client.json', function(erreur, fichier) {
     personne = JSON.parse(fichier);
     console.log(personne);
 });
     res.sendFile(__dirname+"/inscription.html");
-})
+});
 app.get("/contact.html",function(req,res){
 
     res.sendFile(__dirname+"/contact.html");
 });
+
+
+
+
 
 exports.dataJSON = function(){
     return personne;
